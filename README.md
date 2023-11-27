f88810591300716f3ca76e2754b5a9c25b0d78dd
6969 <t6969@6969s-MacBook-Pro.local>
2023-11-27 22:33:59 +0700

    firrst

---
+++ README.md
@@ -0,0 +1,19 @@
+# Word-Cross-Game made with Flutter
+
+A new Flutter game.
+
+![inGame](https://user-images.githubusercontent.com/33134976/87345871-670eb600-c559-11ea-90eb-974daac04408.jpeg)
+
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/t6969/fvm/versions/2.5.0"
+export "FLUTTER_APPLICATION_PATH=/Users/t6969/ios/new1/Word-Cross-Game"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/t6969/ios/new1/Word-Cross-Game/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/t6969/ios/new1/Word-Cross-Game/.dart_tool/package_config.json"
